4ee49a3f63be3cfabc008b33e82dbedf628caea1
JP Mejia <69003047+JPMejia2@users.noreply.github.com>
2021-02-02 21:19:50 -0600

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -4,7 +4,7 @@ var titulo = document.getElementById('saludo');
 function cambiarColor (){
     titulo.classList.remove("saludo");
     titulo.classList.add("nuevaClase")
-}
+};
 
 
 var caja_html = document.getElementById('caja');
@@ -13,20 +13,33 @@ function funcionCaja (){
     
     caja_html.classList.remove("estilo_1");
     caja_html.classList.add("estilo_2")
-}
+};
 
-var grupo = document.activeElementById('ds_btn');
+var cajita = document.getElementById('boton');
+
+function camColor (){
+    cajita.classList.remove("ds_btn");
+    cajita.classList.add("ds_btn2")
+};
+
 
 function grupo (){
     caja_html.classList.remove("estilo_1")
     caja_html.classList.add("estilo_2")
     titulo.classList.remove("saludo")
     titulo.classList.add("nuevaClase")
+    cajita.classList.remove("ds_btn");
+    cajita.classList.add("ds_btn2")
 }
+
 /*-------------------------*/
 
 var btn_html = document.getElementById('boton');
 
 titulo.addEventListener('click', cambiarColor);
 
+btn_html.addEventListener('click',funcionCaja);
+
 btn_html.addEventListener('click',grupo);
+
+btn_html.addEventListener('click',camColor);
